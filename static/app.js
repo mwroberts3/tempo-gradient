6590b954a1d7badc1d74d25f8e4aeafb1fa1c574
Mike <mwroberts89@gmail.com>
2020-08-12 16:18:01 -0400

    version 0.9, need to have it working as regular metronome and tie up some loose ends, plus README

--- static/app.js
+++ static/app.js
@@ -4,6 +4,9 @@ const click = document.querySelector("#metronomeClick");
 const timeDisplay = document.querySelector(".time-display");
 const fractionCount = document.querySelectorAll(".interval-fraction");
 const timeBarDisplay = document.querySelector(".time-bar-display");
+const playBackOptions = document.querySelector(".playback-options");
+const clickSound = document.getElementById("click-sound");
+const clickSoundFile = document.getElementById("metronomeClick");
 let bpmStart = 0;
 let bpmEnd = 0;
 let duration = 0;
@@ -12,9 +15,22 @@ let pivotPoint = 0;
 let currentPivotPointDiff = 0;
 let currentTempo = 0;
 let nowPlaying = false;
+let beatOne = document.querySelector("#beatOne");
+let beatTwo = document.querySelector("#beatTwo");
+let beatOnKick = true;
 
-// listen for bpm and duration settings
-settings.addEventListener("click", () => {
+clickSound.addEventListener("click", () => {
+  console.log(clickSound.value);
+  if (clickSound.value == "drum") {
+    clickSoundFile.src = "static/drum.wav";
+  } else if (clickSound.value == "woodblock") {
+    clickSoundFile.src = "static/woodBlock.wav";
+  } else {
+    clickSoundFile.src = "static/click.wav";
+  }
+});
+
+settings.playStop.addEventListener("click", () => {
   bpmStart = settings.tempostart.value;
   bpmEnd = settings.tempoend.value;
   duration = settings.duration.value;
@@ -50,39 +66,44 @@ settings.addEventListener("click", () => {
       beatDiff
     );
   }
+  if (!nowPlaying) {
+    playButton.blur();
+    playButton.value = "reset";
+    nowPlaying = true;
+    let n = 0;
+    let start = new Date().getTime();
+    let overallClock = new Date().getTime();
+    let firstCheck = false;
+    startPlayback(n, start, overallClock, firstCheck);
+    timeBar();
+  } else {
+    location.reload();
+  }
 });
 
-playButton.addEventListener("click", () => {
-  startPlayback();
-  timeBar();
-});
-
-function startPlayback() {
-  click.play();
-  let n = 0;
-  let pivotFraction = 1;
-  let start = new Date().getTime();
-  let overallClock = new Date().getTime();
-  let fractionCheck = true;
-  let firstCheck = false;
+function startPlayback(n, start, overallClock, firstCheck) {
+  // playBackOptions.classList.toggle("cover");
   const metronome = setInterval(() => {
     if (new Date().getTime() - start >= currentTempo) {
-      click.play();
+      if (settings.beat.checked) {
+        if (beatOnKick) {
+          beatOne.play();
+          beatOnKick = false;
+        } else {
+          beatTwo.play();
+          beatOnKick = true;
+        }
+      } else {
+        click.play();
+      }
       // console.log(new Date().getTime() - start);
       start = new Date().getTime();
-    }
-    if (fractionCheck) {
-      timeDisplay.innerHTML += `<li class="interval-fraction">${n}</li>`;
-      fractionCheck = false;
-    } else if (!fractionCheck) {
-      pivotFraction = timeDisplay.childNodes.length;
-      // console.log(timeDisplay.childNodes.length);
     }
     if (new Date().getTime() - overallClock >= pivotPoint) {
       firstCheck = true;
     }
     if (new Date().getTime() - overallClock >= pivotPoint * n && firstCheck) {
-      currentTempo -= beatDiff / pivotFraction;
+      currentTempo -= beatDiff;
       n += 1;
       console.log("----------", n);
       console.log("current tempo: ", currentTempo);
@@ -94,7 +115,19 @@ function startPlayback() {
       console.log("time elapsed: ", new Date().getTime() - overallClock);
     }
     if (currentTempo <= bpmEnd) {
-      clearInterval(metronome);
+      if (settings.loop.checked) {
+        // console.log("loop checked");
+        n = 0;
+        start = new Date().getTime();
+        overallClock = new Date().getTime();
+        firstCheck = false;
+        currentTempo = 60000 / (60000 / bpmStart);
+      } else {
+        clearInterval(metronome);
+      }
+    }
+    if (settings.beat.checked) {
+      console.log("beat checked");
     }
   }, 10);
 }
@@ -103,10 +136,20 @@ function timeBar() {
   let timeBarCheck = new Date().getTime();
   let timeBarQue = (settings.duration.value * 60000) / 50;
   let k = 1;
-  setInterval(() => {
+  const timeBarConst = setInterval(() => {
     if (new Date().getTime() - timeBarCheck >= timeBarQue * k) {
       timeBarDisplay.innerHTML += `<div class="time-bar-marker"></div>`;
       k += 1;
+    }
+    if (k > 50 && !settings.loop.checked) {
+      clearInterval(timeBarConst);
+      setTimeout(() => {
+        location.reload();
+      }, 3000);
+    } else if (k > 50 && settings.loop.checked) {
+      k = 1;
+      timeBarCheck = new Date().getTime();
+      timeBarDisplay.innerHTML = ``;
     }
     console.log(timeBarQue);
   }, 10);
